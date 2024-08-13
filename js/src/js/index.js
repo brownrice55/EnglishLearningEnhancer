7509b8bf2fd00f299825282f59817b96dd9bf4c9
brownrice55 <113149599+brownrice55@users.noreply.github.com>
2024-08-13 16:39:21 +0900

    feat: add data editing functionality

--- js/src/js/index.js
+++ js/src/js/index.js
@@ -8,6 +8,7 @@
   Activation.prototype.initialize = function() {
     this.section = document.querySelectorAll('section');
     this.pageData = localStorage.getItem('pageData') || '';
+    this.navLiElms = document.querySelectorAll('.js-nav li');
   };
 
   Activation.prototype.selectPage = function() {
@@ -44,6 +45,10 @@
   DataManagement.prototype.initialize = function() {
     this.saveBtnElm = document.querySelector('.js-save');
     this.listElm = document.querySelector('.js-list');
+    this.listTitleElm = document.querySelector('.js-listTitle');
+    this.inputElms = document.querySelectorAll('.js-input');
+    this.listLiElms = document.querySelectorAll('.js-list li');
+
     this.sentencesData = new Map();
     let sentencesData = localStorage.getItem('sentencesData');
     if(sentencesData!=='undefined') {
@@ -54,34 +59,52 @@
   };
 
   DataManagement.prototype.showList = function() {
+    if(this.sentencesData.size) {
+      this.listTitleElm.classList.remove('disp--none');
+    }
+    else {
+      this.listTitleElm.classList.add('disp--none');
+    }
     let showList = '';
     this.sentencesData.forEach((value, key) => {
       showList += '<li data-index="' + key + '">' + value.en + '</li>';
     });
     this.listElm.innerHTML = showList;
+    this.listLiElms = document.querySelectorAll('.js-list li');
   };
 
-  DataManagement.prototype.saveData = function(aEn, aJp, aPath, aNum) {
-    this.sentencesData.set((this.sentencesData.size+1), { en:aEn, jp:aJp, path:aPath, num:aNum});
+  DataManagement.prototype.saveData = function(aIndex, aEn, aJp, aNote, aPath) {
+    for(let cnt=0,len=this.inputElms.length-2;cnt<len;++cnt) {
+      if(!this.inputElms[cnt].value) {
+        return;
+      }
+    }
+    let id = (aIndex=='n') ? this.sentencesData.size+1 : parseInt(aIndex);
+    let numArray = this.inputElms[0].value.split(' ');
+    this.sentencesData.set(id, { en:aEn, jp:aJp, note:aNote, path:aPath, num:numArray.length});
     localStorage.setItem('sentencesData', JSON.stringify([...this.sentencesData]));
-    this.showList();
+    window.location.reload(false);
   };
 
   DataManagement.prototype.setEvent = function() {
     let that = this;
     this.saveBtnElm.addEventListener('click', function() {
-      let inputElms = document.querySelectorAll('.js-input');
-      for(let cnt=0,len=inputElms.length-1;cnt<len;++cnt) {
-        if(!inputElms[cnt].value) {
-          return;
-        }
-      }
-      let numArray = inputElms[0].value.split('');
-      that.saveData(inputElms[0].value, inputElms[1].value, inputElms[2].value, numArray.length);
-      for(let cnt=0,len=inputElms.length;cnt<len;++cnt) {
-        inputElms[cnt].value = '';
-      }
+      that.saveData(this.dataset.index, that.inputElms[0].value, that.inputElms[1].value, that.inputElms[2].value, that.inputElms[3].value);
     });
+    let id = 0;
+    let selectedData = new Map();
+    for(let cnt=0,len=this.listLiElms.length;cnt<len;++cnt) {
+      this.listLiElms[cnt].addEventListener('click', function() {
+        id = this.dataset.index;
+        selectedData = that.sentencesData.get(parseInt(id));
+        that.inputElms[0].value = selectedData.en;
+        that.inputElms[1].value = selectedData.jp;
+        that.inputElms[2].value = selectedData.note;
+        that.inputElms[3].value = selectedData.path;
+        that.saveBtnElm.dataset.index = id;
+        that.saveBtnElm.innerHTML = '上書き保存';
+      });
+    }
   };
 
   DataManagement.prototype.run = function() {
